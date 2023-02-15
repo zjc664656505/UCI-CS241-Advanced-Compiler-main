643dd0e9b1e030e8b600196da9981fe1ae1895da
Zhao, Junchen <junchen.zhao19@imperial.ac.uk>
2023-02-14 21:50:01 -0800

    update if and join block. Work on whileblock and cfg

--- DataStructure/Blocks/IfBlock.py
+++ DataStructure/Blocks/IfBlock.py
@@ -3,4 +3,41 @@ from DataStructure.DataResult.IResult import IResult
 from DataStructure.Blocks.Block import Block
 from DataStructure.Blocks.IBlock import IBlock
 from DataStructure.DataResult.BranchResult import BranchResult
+from DataStructure.Blocks.JoinBlock import JoinBlock
+
+class IfBlock(Block):
+    thenBlock = None
+    elseBlock = None
+    joinBlock = None
+
+    def __init__(self, id:int):
+        super().__init__(id)
+        self.thenBlock = None
+        self.elseBlock = None
+        self.joinBlock = None
+
+    def setThenBlock(self, block:IBlock):
+        self.thenBlock = block
+
+    def getThenBlock(self):
+        return self.thenBlock
+
+    def setElseBlock(self, block:IBlock):
+        self.elseBlock = block
+
+    def getElseBlock(self):
+        return self.elseBlock
+
+    def setJoinBlock(self, block:IBlock):
+        self.joinBlock = block
+
+    def getJoinBlock(self):
+        return self.joinBlock
+
+    def freezBranch(self, iid:int, targetblock:IBlock):
+        instruction = self.getinstruction(iid)
+        if isinstance(instruction.operandy, BranchResult):
+            instruction.operandy.set(targetblock)
+
+
 
--- DataStructure/Blocks/WhileBlock.py
+++ DataStructure/Blocks/WhileBlock.py
@@ -3,6 +3,16 @@ from DataStructure.Manager.PhiManager import PhiManager
 from DataStructure.Manager.VariableManager import VariableManager
 from IR.IrGenerator import IrGenerator
 from DataStructure.DataResult.RegisterResult import RegisterResult
+from util.Constants import Constants
+from DataStructure.Blocks import Block
+from DataStructure.Manager.VariableManager import VariableManager
+from DataStructure.Blocks.IBlock import IBlock
+from DataStructure.Blocks.JoinBlock import JoinBlock
+from multipledispatch import dispatch
+from DataStructure.Instruction import Instruction, InstructionResult
+from DataStructure.Operator import Operator, OperatorCode
+from DataStructure.DataResult.ConstantResult import ConstantResult
+from DataStructure.DataResult.RegisterResult import RegisterResult
 
 
 # TODO: Need to finish Control flow graph first
